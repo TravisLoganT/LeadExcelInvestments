807f4ed0e493b68f763be6c6844a7cae623d2d02
Travis <travislogant@gmail.com>
2023-08-12 00:50:30 +0100

    intialisation of server side

---
+++ server/index.js
@@ -0,0 +1,14 @@
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
